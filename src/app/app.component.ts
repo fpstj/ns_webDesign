28fd79e831a466c696e55096071d8feca8cb3aa3
Filip <stojanov.f@gmail.com>
2023-11-15 14:20:09 +0100

    carousel & images added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NsWebDesignComponent } from './Home/ns-web-design.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NsWebDesignComponent],
+  imports: [CommonModule, RouterOutlet, NsWebDesignComponent, NgbModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
